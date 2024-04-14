1065d890a852065388d80d59f4a17093e80cd03b
Amaan9838 <amaanmohd115@gmail.com>
2024-04-14 23:22:16 +0530

    only amsterdam

--- application.py
+++ application.py
@@ -245,7 +245,7 @@ def reels():
     pr_oxy = [0]
     index = random.choice(pr_oxy)
     proxyDict = {"http" : proxies[index], "https" : proxies[index]}
-    c= [a,d]
+    c= [d]
     cookie_jar = random.choice(c)
     headers = {
             "user-agent": "Mozilla/5.0 (Linux; Android 8.1.0; motorola one Build/OPKS28.63-18-3; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.80 Mobile Safari/537.36 Instagram 72.0.0.21.98 Android (27/8.1.0; 320dpi; 720x1362; motorola; motorola one; deen_sprout; qcom; pt_BR; 132081645)"
@@ -640,7 +640,7 @@ def audio():
     pr_oxy = [0]
     index = random.choice(pr_oxy)
     proxyDict = {"http" : proxies[index], "https" : proxies[index]}
-    c= [a,d]
+    c= [d]
     cookie_jar = random.choice(c)
     headers = {
             "user-agent": "Mozilla/5.0 (Linux; Android 8.1.0; motorola one Build/OPKS28.63-18-3; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.80 Mobile Safari/537.36 Instagram 72.0.0.21.98 Android (27/8.1.0; 320dpi; 720x1362; motorola; motorola one; deen_sprout; qcom; pt_BR; 132081645)"
